187253644d5abe90938ef516e1642865e9140312
Devilzer <jenadeepak724@gmail.com>
2020-12-06 00:43:32 +0530

    add redux store

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 //Action
 const BUY_CAKE = "BUY_CAKE";
 
@@ -14,12 +17,19 @@ const initialState ={
 }
 
 const reducer = (state = initialState ,action)=>{
-    switch (action) {
+    switch (action.type) {
         case BUY_CAKE: return{
             ...state,
             numOfCake : state.numOfCake -1 
-        };
-        default:
-            return state;
+        }
+        default: return state
     }
-}
+};
+
+const store = createStore(reducer);
+console.log("initial State ",store.getState());
+const unsubscribe =store.subscribe(()=>console.log('updated state ',store.getState()));
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
